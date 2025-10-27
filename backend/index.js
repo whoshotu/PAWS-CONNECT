8c573fd5f9eaba30e028df61567779913f22fd7c
Whoshotu <lopezanth661@gmail.com>
2025-10-26 17:04:39 -0700

    fix: Manually set CORS headers

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 
 // Set static folder
